b1f7bd7650932abcf4d21647e8b93652561a78a2
xingyulu0701 <xingyulu0701@berkeley.edu>
2018-04-13 12:30:52 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,287 @@
+import copy
+import glob
+import os
+import time
+
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from arguments import get_args
+from baselines.common.vec_env.dummy_vec_env import DummyVecEnv
+from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from baselines.common.vec_env.vec_normalize import VecNormalize
+from envs import make_env
+from kfac import KFACOptimizer
+from model import CNNPolicy, MLPPolicy, OptionCritic
+from storage import RolloutStorage
+from visualize import visdom_plot
+
+
+
+args = get_args()
+
+assert args.algo in ['a2c', 'ppo', 'acktr']
+if args.recurrent_policy:
+    assert args.algo in ['a2c', 'ppo'], \
+        'Recurrent policy is not implemented for ACKTR'
+
+num_updates = int(args.num_frames) // args.num_steps // args.num_processes
+
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+try:
+    os.makedirs(args.log_dir)
+except OSError:
+    files = glob.glob(os.path.join(args.log_dir, '*.monitor.csv'))
+    for f in files:
+        os.remove(f)
+
+num_options = 2
+
+def main():
+    print("#######")
+    print("WARNING: All rewards are clipped or normalized so you need to use a monitor (see envs.py) or visdom plot to get true rewards")
+    print("#######")
+
+    os.environ['OMP_NUM_THREADS'] = '1'
+
+    if args.vis:
+        from visdom import Visdom
+        viz = Visdom(port=args.port)
+        win = None
+
+    envs = [make_env(args.env_name, args.seed, i, args.log_dir)
+                for i in range(args.num_processes)]
+    # env = get_test_env("001")
+
+    # num_states = len(env.all_possible_states())
+    if args.num_processes > 1:
+        envs = SubprocVecEnv(envs)
+    else:
+        envs = DummyVecEnv(envs)
+
+    if len(envs.observation_space.shape) == 1:
+        envs = VecNormalize(envs)
+
+    obs_shape = envs.observation_space.shape
+    obs_shape = (obs_shape[0] * args.num_stack, *obs_shape[1:])
+
+    if len(envs.observation_space.shape) == 3:
+        actor_critic = OptionCritic(num_options, obs_shape[0], envs.action_space, args.recurrent_policy)
+    else:
+        # assert not args.recurrent_policy, \
+        #     "Recurrent policy is not implemented for the MLP controller"
+        # actor_critic = MLPPolicy(obs_shape[0], envs.action_space)
+        raise NotImplementedError()
+
+    if envs.action_space.__class__.__name__ == "Discrete":
+        action_shape = 1
+    else:
+        action_shape = envs.action_space.shape[0]
+
+    if args.cuda:
+        actor_critic.cuda()
+
+    if args.algo == 'a2c':
+        # optimizer = optim.RMSprop(actor_critic.parameters(), args.lr, eps=args.eps, alpha=args.alpha)
+        raise NotImplementedError()
+    elif args.algo == 'ppo':
+        optionOptimizers = [optim.Adam(actor_critic.intraOption[i].parameters(), args.lr, eps=args.eps) for i in range(num_options)]
+        terminationOptimizers = [optim.Adam(actor_critic.terminationOption[i].parameters(), args.lr, eps=args.eps) for i in range(num_options)]
+        optionSelectionOptimizer = optim.Adam(actor_critic.optionSelection.parameters(), args.lr, eps = args.eps)
+    elif args.algo == 'acktr':
+        # optimizer = KFACOptimizer(actor_critic)
+        raise NotImplementedError()
+
+    rollouts = RolloutStorage(args.num_steps, args.num_processes, obs_shape, envs.action_space, actor_critic.state_size, num_options)
+    current_obs = torch.zeros(args.num_processes, *obs_shape)
+
+    def update_current_obs(obs):
+        shape_dim0 = envs.observation_space.shape[0]
+        obs = torch.from_numpy(obs).float()
+        if args.num_stack > 1:
+            current_obs[:, :-shape_dim0] = current_obs[:, shape_dim0:]
+        current_obs[:, -shape_dim0:] = obs
+
+    obs = envs.reset()
+    update_current_obs(obs)
+
+    rollouts.observations[0].copy_(current_obs)
+
+    # These variables are used to compute average rewards for all processes.
+    episode_rewards = torch.zeros([ 1])
+    final_rewards = torch.zeros([1])
+    optionSelection = 0
+    if args.cuda:
+        current_obs = current_obs.cuda()
+        rollouts.cuda()
+    option = -1 
+    start = time.time()
+    for j in range(num_updates):
+        for step in range(args.num_steps):
+            # Choose Option 
+            if option == -1:
+                selection_value, option, option_log_prob, states = actor_critic.get_option(Variable(rollouts.observations[step], volatile=True),
+                    Variable(rollouts.states[step], volatile=True),
+                    Variable(rollouts.masks[step], volatile=True))
+            print("option is:")
+            print(option)
+
+            # Sample actions
+            value, action, action_log_prob, states = actor_critic.get_output(
+                    option,
+                    Variable(rollouts.observations[step], volatile=True),
+                    Variable(rollouts.states[step], volatile=True),
+                    Variable(rollouts.masks[step], volatile=True))
+            cpu_actions = action.data.squeeze(1).cpu().numpy()
+            print("action is:")
+            print(action)
+
+            # Termination 
+            term_value, termination, termination_log_prob, _ = actor_critic.get_termination(Variable(rollouts.observations[step], volatile=True),
+                    Variable(rollouts.states[step], volatile=True),
+                    Variable(rollouts.masks[step], volatile=True))
+            print("termination is:")
+            print(termination)
+
+            # Obser reward and next obs
+            obs, reward, done, info = envs.step(cpu_actions)
+            reward = torch.from_numpy(np.expand_dims(np.stack(reward), 1)).float()
+            episode_rewards += reward
+
+            newIndex = obs_to_int(obs)
+
+            # If done then clean the history of observations.
+            masks = torch.FloatTensor(0.0 if done_ else 1.0)
+            final_rewards *= masks
+            final_rewards += (1 - masks) * episode_rewards
+            episode_rewards *= masks
+
+            if termination:
+                option = -1 
+
+            if args.cuda:
+                masks = masks.cuda()
+
+            if current_obs.dim() == 4:
+                current_obs *= masks.unsqueeze(2).unsqueeze(2)
+            else:
+                current_obs *= masks
+
+            update_current_obs(obs)
+            rollouts.insert(step, current_obs, states.data, action.data, action_log_prob.data, value.data, reward, masks, option, termination)
+
+        next_value = actor_critic(Variable(rollouts.observations[-1], volatile=True),
+                                  Variable(rollouts.states[-1], volatile=True),
+                                  Variable(rollouts.masks[-1], volatile=True))[0].data
+
+        rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
+
+        if args.algo in ['a2c', 'acktr']:
+            raise NotImplementedError()
+        elif args.algo == 'ppo':
+            advantages = rollouts.returns[:-1] - rollouts.value_preds[:-1]
+            advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+
+            for e in range(args.ppo_epoch):
+                for i in range(args.num_steps):
+                    # Get the ith step during exploration
+                    option = Variable(rollouts.options[i])
+                    adv_targ = Variable(advantages[i])
+                    old_action_log_probs = rollouts.action_log_probs[i]
+                    termination = Variable(rollouts.optionSelection[i])
+                    # Use critic value of option nn to update option parameters
+                    values, action_log_probs, dist_entropy, states = actor_critic.evaluate_option(
+                        Variable(rollouts.observations[i]),
+                        Variable(rollouts.states[i]),
+                        Variable(rollouts.masks[i]),
+                        Variable(rollouts.actions[i]), option)
+                    ratio = torch.exp(action_log_probs - Variable(old_action_log_probs))
+                    surr1 = ratio * adv_targ
+                    surr2 = torch.clamp(ratio, 1.0 - args.clip_param, 1.0 + args.clip_param) * adv_targ
+                    action_loss = -torch.min(surr1, surr2).mean() # PPO's pessimistic surrogate (L^CLIP)
+                    value_loss = (Variable(self.returns[i]) - values).pow(2).mean()
+                    optionOptimizer = optionOptimizers[option]
+                    optionOptimizer.zero_grad()
+                    (action_loss + value_loss).backward()
+                    nn.utils.clip_grad_norm(optionOptimizers.parameters(), args.max_grad_norm)
+                    optionOptimizers.step()
+
+                    # Use critic value of option nn to obtain V_Omega. Not correct at the moment. Need to fix this part.
+                    selection_log_prob = actor_critic.evaluate_selection(
+                        Variable(rollouts.observations[i]),
+                        Variable(rollouts.states[i]),
+                        Variable(rollouts.masks[i]),
+                        option)
+                    V_Omega = selection_log_prob * values 
+
+                    # Update termination parameters 
+                    termination_log_prob = actor_critic.evaluate_termination(
+                        Variable(rollouts.observations[i]),
+                        Variable(rollouts.states[i]),
+                        Variable(rollouts.masks[i]),
+                        termination, 
+                        option)
+                    if termination:
+                        termination_loss = - torch.exp(termination_log_prob) * V_Omega - (1 - torch.exp(termination_log_prob)) * values
+                    else:
+                        termination_loss = - (1 - torch.exp(termination_log_prob)) * V_Omega - torch.exp(termination_log_prob) * values
+                    terminationOptimizer = terminationOptimizers[option]
+                    terminationOptimizer.zero_grad()
+                    (termination_loss).backward()
+                    nn.utils.clip_grad_norm(terminationOptimizers.parameters(), args.max_grad_norm)
+                    terminationOptimizer.step()
+
+                    if i != 0 and rollouts.optionSelection[i - 1]:
+                        optionSelectionOptimizer.zero_grad()
+                        (V_Omega).backward()
+                        nn.utils.clip_grad_norm(optionSelectionOptimizer.parameters(), args.max_grad_norm)
+                        optionSelectionOptimizer.step()
+
+        rollouts.after_update()
+
+        if j % args.save_interval == 0 and args.save_dir != "":
+            save_path = os.path.join(args.save_dir, args.algo)
+            try:
+                os.makedirs(save_path)
+            except OSError:
+                pass
+
+            # A really ugly way to save a model to CPU
+            save_model = actor_critic
+            if args.cuda:
+                save_model = copy.deepcopy(actor_critic).cpu()
+
+            save_model = [save_model,
+                            hasattr(envs, 'ob_rms') and envs.ob_rms or None]
+
+            torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))
+
+        if j % args.log_interval == 0:
+            end = time.time()
+            total_num_steps = (j + 1) * args.num_processes * args.num_steps
+            print("Updates {}, num timesteps {}, FPS {}, mean/median reward {:.1f}/{:.1f}, min/max reward {:.1f}/{:.1f}, entropy {:.5f}, value loss {:.5f}, policy loss {:.5f}".
+                format(j, total_num_steps,
+                       int(total_num_steps / (end - start)),
+                       final_rewards.mean(),
+                       final_rewards.median(),
+                       final_rewards.min(),
+                       final_rewards.max(), dist_entropy.data[0],
+                       value_loss.data[0], action_loss.data[0]))
+        if args.vis and j % args.vis_interval == 0:
+            try:
+                # Sometimes monitor doesn't properly flush the outputs
+                win = visdom_plot(viz, win, args.log_dir, args.env_name,
+                                  args.algo, args.num_frames)
+            except IOError:
+                pass
+
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,237 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from distributions import Categorical, DiagGaussian
+from utils import orthogonal
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1 or classname.find('Linear') != -1:
+        orthogonal(m.weight.data)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+class FFPolicy(nn.Module):
+    def __init__(self):
+        super(FFPolicy, self).__init__()
+
+    def forward(self, inputs, states, masks):
+        raise NotImplementedError
+
+    def act(self, inputs, states, masks, deterministic=False):
+        value, x, states = self(inputs, states, masks)
+        action = self.dist.sample(x, deterministic=deterministic)
+        action_log_probs, dist_entropy = self.dist.logprobs_and_entropy(x, action)
+        return value, action, action_log_probs, states
+
+    def evaluate_option(self, inputs, states, masks, action, option):
+        value, x, states = self.intraOption[option].forward(inputs, states, masks)
+        action_log_probs, dist_entropy = self.intraOption[option].dist.logprobs_and_entropy(x, actions)
+        return value, action_log_probs, dist_entropy, states
+
+    def evaluate_selection(self, inputs, states, masks, option):
+        value, x, states = self.optionSelection.forward(inputs, states, masks)
+        option_log_probs, dist_entropy = self.optionSelection.dist.logprobs_and_entropy(x, option)
+        return option_log_probs
+
+    def evaluate_termination(self, inputs, states, masks, termination, option):
+        value, x, states = self.terminationOption[option].forward(inputs, states, masks)
+        termination_log_probs, dist_entropy = self.terminationOption[option].dist.logprobs_and_entropy(x, termination)
+        return termination_log_probs
+
+class OptionCritic(FFPolicy):
+    @property
+    def state_size(self):
+        if hasattr(self, 'gru'):
+            return 512
+        else:
+            return 1
+    def __init__(self, num_options, num_inputs, action_space, use_gru):
+        super(OptionCritic, self).__init__()
+        self.num_options = num_options
+        self.featureNN = CNNPolicy(num_inputs, use_gru)
+        self.optionSelection = OptionPolicy(64, num_options, None, use_gru)
+        self.intraOption = [OptionPolicy(64, None, action_space, use_gru) for i in range(num_options)]
+        self.terminationOption = [OptionPolicy(64, 1, None, use_gru) for i in range(num_options)]
+        # self.state_values = torch.zeros(num_states)
+
+    def get_output(self, num_option, inputs, states, masks, deterministic= False):
+        features = self.featureNN(inputs, states, masks)
+        option = self.intraOption[num_options]
+        value, x, states = option.forward(features, states, masks)
+        action = option.dist.sample(x, deterministic = deterministic)
+        action_log_probs, dist_entropy = self.dist.logprobs_and_entropy(x, action)
+        return value, action, action_log_probs, states 
+
+    def get_option(self, inputs, states, masks, deterministic = False):
+        features = self.featureNN(inputs, states, masks)
+        value, x, states = self.optionSelection.forward(features, states, masks)
+        option = self.optionSelection.dist.sample(x, deterministic = deterministic)
+        option_log_probs, dist_entropy = self.optionSelection.dist.logprobs_and_entropy(x, option)
+        return value, option, option_log_probs, states 
+
+    def get_termination(self, num_option, inputs, states, masks, deterministic = False):
+        features = self.featureNN(inputs, states, masks)
+        option = self.terminationOption[num_options]
+        value, x, states = option.forward(features, states, masks)
+        action = option.dist.sample(x, deterministic = deterministic)
+        action_log_probs, dist_entropy = self.dist.logprobs_and_entropy(x, action)
+        return value, action, action_log_probs, states 
+
+class OptionPolicy(FFPolicy):
+    def __init__(self, num_inputs, num_outputs, action_space, use_gru):
+        super(OptionPolicy, self).__init__()
+        if num_outputs == None:
+            if action_space.__class__.__name__ == "Discrete":
+                num_outputs = action_space.n
+                self.dist = Categorical(512, num_outputs)
+            elif action_space.__class__.__name__ == "Box":
+                num_outputs = action_space.shape[0]
+                self.dist = DiagGaussian(512, num_outputs)
+            else:
+                raise NotImplementedError
+        else:
+            self.dist = Categorical(512, num_outputs)
+        self.conv1 = nn.Conv2d(64, 64, 3, stride = 1)
+        self.linear1 = nn.Linear(32*7*7, 512)
+        self.linear_critic = nn.Linear(512, 1)
+        self.train()
+        self.reset_parameters()
+
+    
+    @property
+    def state_size(self):
+        if hasattr(self, 'gru'):
+            return 512
+        else:
+            return 1
+
+    def reset_parameters(self):
+        self.apply(weights_init)
+
+        relu_gain = nn.init.calculate_gain('relu')
+        self.conv1.weight.data.mul_(relu_gain)
+        self.linear1.weight.data.mul_(relu_gain)
+        if self.dist.__class__.__name__ == "DiagGaussian":
+            self.dist.fc_mean.weight.data.mul_(0.01)
+
+    def forward(self, inputs, states, masks):
+        x = self.conv1(inputs / 255.0)
+        x = F.relu(x)
+        x = x.view(-1, 32 * 7 * 7)
+        x = self.linear1(x)
+        x = F.relu(x)
+        value = self.linear_critic(x)
+
+        return value, x, states
+
+class CNNPolicy(FFPolicy):
+    def __init__(self, num_inputs, use_gru):
+        super(CNNPolicy, self).__init__()
+        self.conv1 = nn.Conv2d(num_inputs, 32, 8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+
+        self.train()
+        self.reset_parameters()
+
+    @property
+    def state_size(self):
+        if hasattr(self, 'gru'):
+            return 512
+        else:
+            return 1
+
+    def reset_parameters(self):
+        self.apply(weights_init)
+
+        relu_gain = nn.init.calculate_gain('relu')
+        self.conv1.weight.data.mul_(relu_gain)
+        self.conv2.weight.data.mul_(relu_gain)
+        self.conv3.weight.data.mul_(relu_gain)
+        
+
+    def forward(self, inputs, states, masks):
+        x = self.conv1(inputs / 255.0)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+
+        return x, states
+
+
+def weights_init_mlp(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        m.weight.data.normal_(0, 1)
+        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+class MLPPolicy(FFPolicy):
+    def __init__(self, num_inputs, action_space):
+        super(MLPPolicy, self).__init__()
+
+        self.action_space = action_space
+
+        self.a_fc1 = nn.Linear(num_inputs, 64)
+        self.a_fc2 = nn.Linear(64, 64)
+
+        self.v_fc1 = nn.Linear(num_inputs, 64)
+        self.v_fc2 = nn.Linear(64, 64)
+        self.v_fc3 = nn.Linear(64, 1)
+
+        if action_space.__class__.__name__ == "Discrete":
+            num_outputs = action_space.n
+            self.dist = Categorical(64, num_outputs)
+        elif action_space.__class__.__name__ == "Box":
+            num_outputs = action_space.shape[0]
+            self.dist = DiagGaussian(64, num_outputs)
+        else:
+            raise NotImplementedError
+
+        self.train()
+        self.reset_parameters()
+
+    @property
+    def state_size(self):
+        return 1
+
+    def reset_parameters(self):
+        self.apply(weights_init_mlp)
+
+        """
+        tanh_gain = nn.init.calculate_gain('tanh')
+        self.a_fc1.weight.data.mul_(tanh_gain)
+        self.a_fc2.weight.data.mul_(tanh_gain)
+        self.v_fc1.weight.data.mul_(tanh_gain)
+        self.v_fc2.weight.data.mul_(tanh_gain)
+        """
+
+        if self.dist.__class__.__name__ == "DiagGaussian":
+            self.dist.fc_mean.weight.data.mul_(0.01)
+
+    def forward(self, inputs, states, masks):
+        x = self.v_fc1(inputs)
+        x = F.tanh(x)
+
+        x = self.v_fc2(x)
+        x = F.tanh(x)
+
+        x = self.v_fc3(x)
+        value = x
+
+        x = self.a_fc1(inputs)
+        x = F.tanh(x)
+
+        x = self.a_fc2(x)
+        x = F.tanh(x)
+
+        return value, x, states
